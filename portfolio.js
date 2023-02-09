b041679d1ed932dbf74540fd16275be66966b089
Alex Omaset <alexomaset711@gmail.com>
2023-02-09 21:46:39 +0300

    add initial changes

--- portfolio.js
+++ portfolio.js
@@ -8,9 +8,69 @@ const API_KEY =
 const CSV_FILE_LOCATION = "./transactions.csv";
 
 // function to get exchange rate
-async function getExchangeRate(from, to){
-    const response = await axios.get(
-        `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}&api_key=${API_KEY}`
-      );
-      return response.data[to];
+async function getExchangeRate(from, to) {
+  const response = await axios.get(
+    `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}&api_key=${API_KEY}`
+  );
+  return response.data[to];
 }
+
+async function getPortfolioValue(date = null, token = null) {
+  let portfolio = {};
+  let latestDate = "";
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(CSV_FILE_LOCATION)
+      .pipe(csv())
+      .on("data", (data) => {
+        const transactionDate = moment
+          .unix(data.timestamp)
+          .format("YYYY-MM-DD");
+        if (!portfolio[data.token]) {
+          portfolio[data.token] = 0;
+        }
+
+        if (data.transaction_type === "DEPOSIT") {
+          portfolio[data.token] += parseFloat(data.amount);
+        } else {
+          portfolio[data.token] -= parseFloat(data.amount);
+        }
+
+        if (!latestDate || moment(transactionDate).isAfter(latestDate)) {
+          latestDate = transactionDate;
+        }
+      })
+      .on("end", async () => {
+        let results = [];
+        for (const t in portfolio) {
+          if (!token || t === token) {
+            const exchangeRateUSD = await getExchangeRate(t, "USD");
+            results.push({
+              token: t,
+              value: (portfolio[t] * exchangeRateUSD).toFixed(2),
+              date: date || latestDate,
+            });
+          }
+        }
+        if (date) {
+          results = results.filter((r) => r.date === date);
+        }
+        resolve(results);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+async function displayPortfolioValue(date, token) {
+  try {
+    const portfolioValue = await getPortfolioValue(date, token);
+    console.log(portfolioValue);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const [, , date, token] = process.argv;
+displayPortfolioValue(date, token);
