39d0502e8bed572e61efe29797f4db9cdedf5976
Alex Omaset <alexomaset711@gmail.com>
2023-02-09 19:53:01 +0300

    exchange rate function

--- portfolio.js
+++ portfolio.js
@@ -6,3 +6,11 @@ const moment = require("moment");
 const API_KEY =
   "2280f1ae742c5726d757ed667e9c2a276eaa9c25b2fb7e6ddefd3663e87eef3b";
 const CSV_FILE_LOCATION = "./transactions.csv";
+
+// function to get exchange rate
+async function getExchangeRate(from, to){
+    const response = await axios.get(
+        `https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}&api_key=${API_KEY}`
+      );
+      return response.data[to];
+}
